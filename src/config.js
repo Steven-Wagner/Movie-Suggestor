7f103b852bb98ac085b3d8b63f310f4f797151a7
Steven Wagner <steven.glen.wagner@gmail.com>
2019-04-26 21:59:50 -0600

    fixed API_BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     API_BASE_URL: 
-        // process.env.REACT_APP_BASE_URL ||
+        process.env.REACT_APP_BASE_URL ||
         "http://localhost:8000/api",
     TOKEN_KEY: 'authToken'
 }
