641116c34caf6f1bcba7d65d8906ce20de226c9f
Steven Wagner <steven.glen.wagner@gmail.com>
2019-05-16 15:52:11 -0600

    added animations and fixes to main-banner

--- src/commonComponents/main-banner.js
+++ src/commonComponents/main-banner.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default function MainBanner(props){
     return (
         <header role="banner" className="main-banner">
-            <h1 ><span className="fas fa-film"></span>Movie Suggestor</h1>
-            <h2 className="subtitle">Personalized Movie Suggestions</h2>
+            <div className="movie-headers">
+                <h1 ><span className="fas fa-film icon"></span>Movie Suggestor</h1>
+                <h2 className="subtitle">Personalized Movie Suggestions</h2>
+            </div>
         </header>
     )
 }
