f486d3a36bab965c4e28254d9cdcbf801b31f89c
Steven Wagner <steven.glen.wagner@gmail.com>
2019-05-16 15:58:41 -0600

    fixed main-banner

--- src/commonComponents/main-banner.js
+++ src/commonComponents/main-banner.js
@@ -3,10 +3,8 @@ import React from 'react';
 export default function MainBanner(props){
     return (
         <header role="banner" className="main-banner">
-            <div className="movie-headers">
-                <h1 ><span className="fas fa-film icon"></span>Movie Suggestor</h1>
-                <h2 className="subtitle">Personalized Movie Suggestions</h2>
-            </div>
+            <h1 ><span className="fas fa-film icon"></span>Movie Suggestor</h1>
+            <h2 className="subtitle">Personalized Movie Suggestions</h2>
         </header>
     )
 }
